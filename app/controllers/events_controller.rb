43c098ee9d475539432f6f1a91042041cf30f050
bvizintin <boris.vizintin@gmail.com>
2021-06-12 19:04:34 +0200

    Working association

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,16 @@
 class EventsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorize_owner!, only: [:edit, :update, :destroy]
+
   def new
     @event = Event.new
   end
 
   def create
     @event = Event.new(event_params)
+    @event.organizer = current_user
+
     if @event.save
       flash[:notice] = "Event je kreiran!"
       redirect_to @event
@@ -53,6 +59,16 @@ class EventsController < ApplicationController
   private
     def event_params
       params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
+    end
+
+    def authorize_owner!
+      @event = Event.find(params[:id])
+      authenticate_user!
+
+      unless @event.organizer == current_user
+        flash[:alert] = "You don't have permission to #{action_name} the #{@event.title.upcase}"
+        redirect_to events_path
+      end
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_12_092123) do
+ActiveRecord::Schema.define(version: 2021_06_12_103441) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_06_12_092123) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_06_12_092123) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
