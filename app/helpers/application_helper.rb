9fc188735f9fc736cab86c524f9736a018075fe3
bvizintin <boris.vizintin@gmail.com>
2021-06-14 20:16:49 +0200

    dalje

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
       "#{page_title} :: #{default_title}"
     end
   end
+
+  def event_status(event)
+    if Time.now < event.start_date
+      return content_tag(:span, "", class: "upcoming") + "Upcoming"
+    elsif Time.now > event.start_date && Time.now < event.end_date
+      return content_tag(:span, "", class: "upcoming") + "In duration"
+    else
+      return content_tag(:span, "", class: "past") + " Past event"
+    end
+  end
+
+
+
 end
