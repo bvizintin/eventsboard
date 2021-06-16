3f8d99f39bf1037b6c829fb205c4f9c700cee983
bvizintin <boris.vizintin@gmail.com>
2021-06-16 18:28:44 +0200

    added admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_12_185428) do
+ActiveRecord::Schema.define(version: 2021_06_16_160823) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_06_12_185428) do
     t.string "last_name"
     t.string "username"
     t.string "organization_name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
