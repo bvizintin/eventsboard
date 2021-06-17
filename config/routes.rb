cd98f02de4d546f70f778c0b8dfac62b7c4e6d57
bvizintin <boris.vizintin@gmail.com>
2021-06-17 15:15:17 +0200

    admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+    namespace :admin do
+      root "application#index"
+      resources :users, only: [:index]
+    end
+
     devise_for :users
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
