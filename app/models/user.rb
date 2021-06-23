d6d676f4826a14b978896e221f65899607203923
bvizintin <boris.vizintin@gmail.com>
2021-06-23 16:31:06 +0200

    more associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+  validates :username, presence: true, length: {minimum: 6}
 
   def full_name
     "#{first_name} #{last_name}"
