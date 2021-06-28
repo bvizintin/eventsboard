9b970b472f7f33b8971c4f3b3ba9bb08ef8dac35
bvizintin <boris.vizintin@gmail.com>
2021-06-28 20:51:12 +0200

    added categories and attendeances

---
+++ app/controllers/admin/attendances_controller.rb
@@ -0,0 +1,6 @@
+class Admin::AttendancesController < Admin::ApplicationController
+  def index
+    @attendances = Attendance.order(created_at: :desc)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     root "application#index"
     resources :users, only: [:index]
     resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :comments, only: [:index]
+    resources :attendances, only: [:index]
   end
     devise_for :users
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
