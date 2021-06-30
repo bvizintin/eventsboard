b2fbd50630d0c40a20c248e103873ed8a53d5c42
bvizintin <boris.vizintin@gmail.com>
2021-06-30 20:56:45 +0200

    tags

---
+++ app/models/tag.rb
@@ -0,0 +1,7 @@
+class Tag < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name
+
+  has_many :taggings, dependent: :destroy
+  has_many :events, through: :taggings
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_170107) do
+ActiveRecord::Schema.define(version: 2021_06_30_172726) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "event_id", null: false
@@ -80,6 +80,21 @@ ActiveRecord::Schema.define(version: 2021_06_29_170107) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_taggings_on_event_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -104,4 +119,6 @@ ActiveRecord::Schema.define(version: 2021_06_29_170107) do
   add_foreign_key "events", "users"
   add_foreign_key "likes", "events"
   add_foreign_key "likes", "users"
+  add_foreign_key "taggings", "events"
+  add_foreign_key "taggings", "tags"
 end
